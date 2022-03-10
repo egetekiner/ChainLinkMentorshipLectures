cb1443a98a63a8533c3df9b34c00baf45b7c5e69
Ege Tekiner <egetekiner@gmail.com>
2022-03-10 11:03:28 -0500

    Add files via upload

---
+++ first_week/Wecome.sol
@@ -0,0 +1,17 @@
+pragma solidity >=0.6.0 <0.9.0;
+
+contract Welcome {
+
+    uint256 CLNumber = 5;
+    bool CLbool = false;
+    string CLstring = "We love mentorship program";
+    int256 clInt = -5;
+    uint8 CLNumberSmall = 5;
+    address CLAdress = 0xFCEc40AcB0a6a21fE8534EB55BEBcB38214b56c9;
+    bytes32 CLByte = "ChainLink"; //byte can be at most 32 byte
+
+    // For more information about the variable types; You can use 
+    // https://docs.soliditylang.org/en/v0.8.11/types.html
+
+
+}
